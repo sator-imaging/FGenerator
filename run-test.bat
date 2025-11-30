87168125fcdb06a69b76b8aab9c7aec84d4f03d6
sator-imaging <16752340+sator-imaging@users.noreply.github.com>
2025-11-30 15:29:44 +0900

    test: test with `#:sdk`

--- run-test.bat
+++ run-test.bat
@@ -1,9 +1,13 @@
 @echo off
 
 dotnet run --project cli -- build ^
+    "test/*.cs" -f -o "./.z__merged_dll__" ^
+    || (echo === ERROR === & exit 1)
+
+dotnet run --project cli -- build ^
     %* ^
-    --unity --force --output  ./.z__merged_dll__ ^
-    ./sample/*.cs ^
+    --unity --force --output "./.z__merged_dll__" ^
+    "./sample/*.cs" ^
     || (echo === ERROR === & exit 1)
 
 dotnet clean         sample/SampleConsumer  || (echo === ERROR === & exit 1)
---
+++ test/build-with-sdk.cs
@@ -0,0 +1,20 @@
+#:sdk FGenerator.Sdk@0.3.0
+
+using FGenerator;
+using Microsoft.CodeAnalysis;
+
+[Generator]
+public class BuiltWithSdkDirectiveGenerator : FGeneratorBase
+{
+    protected override string DiagnosticCategory => "BuiltWithSdkDirectiveGenerator";
+    protected override string DiagnosticIdPrefix => "BWSDK";
+
+    protected override string? TargetAttributeName => "BuiltWithSdkDirectiveAttribute";
+    protected override string? PostInitializationOutput => "// Built with FGenerator.Sdk";
+
+    protected override CodeGeneration? Generate(Target target, out AnalyzeResult? diagnostic)
+    {
+        diagnostic = null;
+        return new CodeGeneration(target.ToHintName(), "// Built with FGenerator.Sdk");
+    }
+}
