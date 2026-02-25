568a4fcfae8e17132775dee9731064f190d9c15e
sator-imaging <16752340+sator-imaging@users.noreply.github.com>
2026-02-25 10:28:24 +0900

    Apply suggestion from @sator-imaging

--- test/DiscoverTest.cs
+++ test/DiscoverTest.cs
@@ -33,9 +33,9 @@ internal sealed class DiscoverAttribute : Attribute { }
         var sb = new StringBuilder();
         if (isTypeOrMember) sb.AppendLine(target.ToNamespaceAndContainingTypeDeclarations());
         sb.AppendLine("/*");
-        sb.AppendLine($"ToDeclarationString: {target.ToDeclarationString()}");
-        sb.AppendLine($"ToNameString: {target.ToNameString()}");
-        sb.AppendLine($"ToAssemblyUniqueIdentifier: {target.ToAssemblyUniqueIdentifier()}");
+sb.AppendLine($"    {target.ToDeclarationString()}  // ToDeclarationString");
+sb.AppendLine($"    {target.ToNameString()}  // ToNameString");
+sb.AppendLine($"    {target.ToAssemblyUniqueIdentifier()}  // ToAssemblyUniqueIdentifier");
         sb.AppendLine("*/");
         if (isTypeOrMember) sb.AppendLine(target.ToNamespaceAndContainingTypeClosingBraces());
 
